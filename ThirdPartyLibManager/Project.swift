244e7aa56fb3f1135f01b53c07371ac1f7d0e41e
kimxwan <kimxwan0319@naver.com>
2021-12-28 16:00:48 +0900

    ⚙️ :: 필요한 외부라이브러리 설치

--- ThirdPartyLibManager/Project.swift
+++ ThirdPartyLibManager/Project.swift
@@ -3,6 +3,14 @@ import ProjectDescriptionHelpers
 
 let project = Project.dynamicFramework(
     name: "ThirdPartyLibManager",
-    packages: [
+    dependencies: [
+        .SPM.RxSwift,
+        .SPM.Realm,
+        .SPM.RxCoca,
+        .SPM.RxMoya,
+        .SPM.Then,
+        .SPM.Swinject,
+        .SPM.PinLayout,
+        .SPM.FlexLayout
     ]
 )
