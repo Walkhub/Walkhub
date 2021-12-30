d058564f92a398e61f0a724f345b384e945af5ac
kimxwan <kimxwan0319@naver.com>
2021-12-31 00:59:12 +0900

    ⚡️ :: Firebase 기본 세팅

--- iOS/Sources/Application/AppDelegate.swift
+++ iOS/Sources/Application/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -7,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
