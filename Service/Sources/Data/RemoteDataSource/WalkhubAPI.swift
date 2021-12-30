f48a750db5633cdf58b130029bad55e0a53efa50
kimxwan <kimxwan0319@naver.com>
2021-12-30 16:45:33 +0900

    ⚡️ :: Moya 기본 세팅

--- Service/Sources/Data/RemoteDataSource/WalkhubAPI.swift
+++ Service/Sources/Data/RemoteDataSource/WalkhubAPI.swift
@@ -1,1 +1,45 @@
+import Foundation
+import Moya
 
+enum WalkhubAPI {
+}
+
+extension WalkhubAPI: TargetType {
+
+    var baseURL: URL {
+        return URL(string: "https:// ")!
+    }
+
+    var path: String {
+        switch self {
+        default:
+            return "/"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        default:
+            return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        default:
+            return .requestPlain
+        }
+    }
+
+    var validationType: ValidationType {
+        return .successCodesㅇ
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+
+}
