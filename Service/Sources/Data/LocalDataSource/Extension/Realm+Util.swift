f81362efe72ba95414d6a9fab6e1c722545733d5
kimxwan <kimxwan0319@naver.com>
2022-01-04 16:05:09 +0900

    📑 :: 누락된 파일 추가

---
+++ Service/Sources/Data/LocalDataSource/Extension/Realm+Util.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+import RealmSwift
+
+extension Results {
+    public func toArray() -> [Element] {
+        return Array(self)
+    }
+    
+    public func toArray<T>(type: T.Type) -> [T] {
+        return compactMap { $0 as? T }
+    }
+}
