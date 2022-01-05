731e3ad93719409bca00b6772edc00b9e279aefa
kimxwan <kimxwan0319@naver.com>
2022-01-05 12:46:13 +0900

    ⚡️ :: AppDelegate에 Container 선언

--- iOS/Sources/Application/AppDelegate.swift
+++ iOS/Sources/Application/AppDelegate.swift
@@ -1,7 +1,15 @@
 import UIKit
 
+import Swinject
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    static var continer: Container {
+        let continer = Container()
+        continer.registerServiceDependencies()
+        return continer
+    }
 
     func application(
         _ application: UIApplication,
