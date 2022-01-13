9ded3a263f006601f0921cb8228e8ff5b47b74a9
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 16:43:48 +0900

    ⚡️::UserRankDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/UserRankDTO.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct UserRankDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case myRank = "my_rank"
+        case rankList = "rank_list"
+    }
+    let myRank: MyRank
+    let rankList: [RankList]
+}
+
+extension UserRankDTO {
+    struct MyRank: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case userID = "user_id"
+            case name
+            case rank
+            case profileImageUrlString = "profile_image_url"
+            case walkCount = "walk_count"
+        }
+        let userID: Int
+        let name: String
+        let rank: Int
+        let profileImageUrlString: String
+        let walkCount: Int
+    }
+    struct RankList: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case userID = "user_id"
+            case name
+            case profileImageUrlString = "profile_image_url"
+            case walkCount = "walk_count"
+        }
+        let userID: Int
+        let name: String
+        let profileImageUrlString: String
+        let walkCount: Int
+    }
+}
