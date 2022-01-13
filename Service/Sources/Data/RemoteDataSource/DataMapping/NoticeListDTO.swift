86e0464606697f32892dc8e892947a4b42953a79
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 16:17:42 +0900

    ⚡️::NoticeListDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/NoticeListDTO.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct NoticeListDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case noticeList = "notice_list"
+    }
+    let noticeList: [Notice]
+}
+
+extension NoticeListDTO {
+    struct Notice: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case id
+            case title
+            case createdAt = "created_at"
+            case writer
+        }
+        let id: Int
+        let title: String
+        let createdAt: String
+        let writer: Writer
+    }
+    struct Writer: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case profileImageUrlString = "profile_image_url"
+        }
+        let id: Int
+        let name: String
+        let profileImageUrlString: String
+    }
+}
