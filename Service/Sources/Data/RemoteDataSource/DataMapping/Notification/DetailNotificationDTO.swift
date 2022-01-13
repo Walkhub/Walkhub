f9761f1656db06d241e30a182bc7aa0b35330c3d
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 14:53:17 +0900

    ⚡️ ::DetailNotificationDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/Notification/DetailNotificationDTO.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct DetailNotificationDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case content
+        case createdAt = "created_at"
+        case isMine
+        case writer
+    }
+    let title: String
+    let content: String
+    let createdAt: String
+    let isMine: Bool
+    let writer: Writer
+}
+
+extension DetailNotificationDTO {
+    struct Writer: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case profileImageUrlString = "profile_image_url"
+        }
+        let id: Int
+        let name: String
+        let profileImageUrlString: String
+    }
+}
+     
