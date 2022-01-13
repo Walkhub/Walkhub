ba8e7261b62a30dbad0a718e3d49c1c9cd3b6cfa
kimxwan <kimxwan0319@naver.com>
2022-01-14 08:37:51 +0900

    ⚡️ :: AuthRepository protocol 작성

--- Service/Sources/Domain/Repository/AuthRepository.swift
+++ Service/Sources/Domain/Repository/AuthRepository.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+import RxSwift
+
+protocol AuthRepository {
+    func singing(id: String, password: String, deviceToken: String) -> Single<Void>
+    func singing(id: String, password: String, name: String, phoneNumber: String, authCode: String) -> Single<Void>
+    func verificationPhone(phoneNumber: String) -> Single<Void>
+}
