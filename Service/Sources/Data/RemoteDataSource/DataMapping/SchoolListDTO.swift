7bf6c41e7e7f6431c6e0cedfcae1cd25bdbd8e3e
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 17:04:38 +0900

    ⚡️::SchoolListDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/SchoolListDTO.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct SchoolListDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case list = "school_list"
+    }
+    let list: [School]
+}
+
+extension SchoolListDTO {
+    struct School: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case agencyCode = "agency_code"
+            case name
+            case rank
+            case logoImageUrlString = "logo_image_url"
+            case walkCount = "walk_count"
+        }
+        let agencyCode: String
+        let name: String
+        let rank: Int
+        let logoImageUrlString: String
+        let walkCount: Int
+    }
+}
