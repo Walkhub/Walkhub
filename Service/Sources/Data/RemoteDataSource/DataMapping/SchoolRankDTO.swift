e0d7db5f71bc9867e646f759afd1197530df6c60
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 16:43:26 +0900

    ⚡️::SchoolRankDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/SchoolRankDTO.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct SchoolRankDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case mySchoolRank = "my_rank"
+        case schoolList = "school_list"
+    }
+    let mySchoolRank: SchoolRank
+    let schoolList: [SchoolList]
+}
+
+extension SchoolRankDTO {
+    struct SchoolRank: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case agencyCode = "agency_code"
+            case name
+            case rank
+            case logoImageUrlString = "logo_image_url"
+            case walkCount = "walk_count"
+        }
+        let agencyCode: String
+        let name: String
+        let rank: Int
+        let logoImageUrlString: String
+        let walkCount: Int
+    }
+    struct SchoolList: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case agencyCode = "agency_code"
+            case name
+            case logoImageUrlString = "logo_image_url"
+            case walkCount = "walk_count"
+        }
+        let agencyCode: String
+        let name: String
+        let logoImageUrlString: String
+        let walkCount: Int
+    }
+}
