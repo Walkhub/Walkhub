5da28b1ac38531148a0530c91cacc3299963ebcf
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 22:37:20 +0900

    ⚡️::Writer속성 -> 새로 만든 WriterDTO로 변경

--- Service/Sources/Data/RemoteDataSource/DataMapping/DetailChallengeDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/DetailChallengeDTO.swift
@@ -24,18 +24,5 @@ struct DetailChallenge: Decodable {
     let scope: String
     let count: Int
     let isMine: Bool
-    let writer: Writer
-}
-
-extension DetailChallenge {
-    struct Writer: Decodable {
-        private enum CodingKeys: String, CodingKey {
-            case id
-            case name
-            case profileImageUrlString = "profile_image_url"
-        }
-        let id: Int
-        let name: String
-        let profileImageUrlString: String
-    }
+    let writer: WriterDTO
 }
