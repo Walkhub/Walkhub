251b56e59bc25ef39866140b7ed66f1cdeb0e3cd
kimxwan <kimxwan0319@naver.com>
2022-01-13 23:31:59 +0900

    ⚡️ :: AuthAPI 정의

--- Service/Sources/Data/RemoteDataSource/API/Auth/AuthAPI.swift
+++ Service/Sources/Data/RemoteDataSource/API/Auth/AuthAPI.swift
@@ -3,8 +3,9 @@ import Foundation
 import Moya
 
 enum AuthAPI {
-    case signin
-    case signup
+    case signin(id: String, password: String, deviceToken: String)
+    case signup(id: String, password: String, name: String, phoneNumber: String, authCode: String)
+    case verificationPhone(phoneNumber: String)
     case renewalToken
 }
 
@@ -20,6 +21,8 @@ extension AuthAPI: WalkhubAPI {
             return "/token"
         case .signup:
             return "/"
+        case .verificationPhone:
+            return "verification-codes"
         case .renewalToken:
             return "/reissue"
         }
@@ -27,7 +30,7 @@ extension AuthAPI: WalkhubAPI {
 
     var method: Moya.Method {
         switch self {
-        case .signin, .signup:
+        case .signin, .signup, .verificationPhone:
             return .post
         case .renewalToken:
             return .put
@@ -36,6 +39,32 @@ extension AuthAPI: WalkhubAPI {
 
     var task: Task {
         switch self {
+        case .signin(let id, let password, let deviceToken):
+            return .requestParameters(
+                parameters: [
+                    "account_id": id,
+                    "password": password,
+                    "device_token": deviceToken
+                ],
+                encoding: JSONEncoding.default
+            )
+        case .signup(let id, let password, let name, let phoneNumber, let authCode):
+            return .requestParameters(
+                parameters: [
+                    "account_id": id,
+                    "password": password,
+                    "name": name,
+                    "phone_number": phoneNumber,
+                    "auth_code": authCode
+                ],
+                encoding: JSONEncoding.default
+            )
+        case .verificationPhone(let phoneNumber):
+            return .requestParameters(
+                parameters: [
+                    "phone_number": phoneNumber
+                ],
+                encoding: JSONEncoding.default)
         default:
             return .requestPlain
         }
