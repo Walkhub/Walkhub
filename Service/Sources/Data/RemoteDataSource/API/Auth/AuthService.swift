15105308954d8f26cd9986a11310991a79481973
kimxwan <kimxwan0319@naver.com>
2022-01-13 23:50:11 +0900

    ⚡️ :: AuthService 작성

--- Service/Sources/Data/RemoteDataSource/API/Auth/AuthService.swift
+++ Service/Sources/Data/RemoteDataSource/API/Auth/AuthService.swift
@@ -9,6 +9,38 @@ final class AuthService: BaseService<AuthAPI> {
 
     private override init() {}
 
+    func signin(
+        id: String,
+        password: String,
+        deviceToken: String
+    ) -> Single<Response> {
+        return request(.signin(
+            id: id,
+            password: password,
+            deviceToken: ""
+        ))
+    }
+
+    func signup(
+        id: String,
+        password: String,
+        name: String,
+        phoneNumber: String,
+        authCode: String
+    ) -> Single<Response> {
+        return request(.signup(
+            id: id,
+            password: password,
+            name: name,
+            phoneNumber: phoneNumber,
+            authCode: authCode
+        ))
+    }
+
+    func verificationPhone(phoneNumber: String) -> Single<Response> {
+        return request(.verificationPhone(phoneNumber: phoneNumber))
+    }
+
     func renewalToken() -> Single<Response> {
         return request(.renewalToken)
     }
