3247f828292f68685d1f4ec2de7d064fd7bd8719
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 11:32:29 +0900

    ⚡️ ::UserProfileDTO 프로퍼티 이름 변경

--- Service/Sources/Data/RemoteDataSource/DataMapping/UserProfileDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/UserProfileDTO.swift
@@ -3,14 +3,14 @@ import Foundation
 struct UserProfileDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case name
-        case profileImg = "profile_image_url"
+        case profileImageString = "profile_image_url"
         case school = "school_name"
         case grade
         case classNum
         case titleBadge = "title_badge"
     }
     let name: String
-    let profileImg: String
+    let profileImageString: String
     let school: String
     let grade: Int
     let classNum: Int
