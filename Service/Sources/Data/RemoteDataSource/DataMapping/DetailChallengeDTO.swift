7ed5d57c6b0cc74fa053a0cbaf403ae5191300f5
KimKiYoung <rlarldud6482@naver.com>
2022-01-13 14:34:50 +0900

    ⚡️ ::DetailChallengeDTO 설정

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/DetailChallengeDTO.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+struct DetailChallengeDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case content
+        case goal
+        case award
+        case imageUrlString = "image_url"
+        case start = "start_at"
+        case end = "end_at"
+        case scope
+        case count = "participant_count"
+        case isMine
+        case writer
+    }
+    let name: String
+    let content: String
+    let goal: Int
+    let award: String
+    let imageUrlString: String
+    let start: String
+    let end: String
+    let scope: String
+    let count: Int
+    let isMine: Bool
+    let writer: Writer
+}
+
+extension DetailChallengeDTO {
+    struct Writer: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case profileImageUrlString = "profile_image_url"
+        }
+        let id: String
+        let name: String
+        let profileImageUrlString: String
+    }
+}
