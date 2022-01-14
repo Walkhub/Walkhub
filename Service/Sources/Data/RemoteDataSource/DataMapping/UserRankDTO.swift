48bc6be9e0c1325af4193d994f2e8e75e52f1c12
KimKiYoung <rlarldud6482@naver.com>
2022-01-14 11:05:31 +0900

    ⚡️::UserDTO로 변환한 것으로 가져다가 씀

--- Service/Sources/Data/RemoteDataSource/DataMapping/UserRankDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/UserRankDTO.swift
@@ -5,35 +5,6 @@ struct UserRankDTO: Decodable {
         case myRank = "my_rank"
         case rankList = "rank_list"
     }
-    let myRank: MyRank
-    let rankList: [Rank]
-}
-
-extension UserRankDTO {
-    struct MyRank: Decodable {
-        private enum CodingKeys: String, CodingKey {
-            case userID = "user_id"
-            case name
-            case rank
-            case profileImageUrlString = "profile_image_url"
-            case walkCount = "walk_count"
-        }
-        let userID: Int
-        let name: String
-        let rank: Int
-        let profileImageUrlString: String
-        let walkCount: Int
-    }
-    struct Rank: Decodable {
-        private enum CodingKeys: String, CodingKey {
-            case userID = "user_id"
-            case name
-            case profileImageUrlString = "profile_image_url"
-            case walkCount = "walk_count"
-        }
-        let userID: Int
-        let name: String
-        let profileImageUrlString: String
-        let walkCount: Int
-    }
+    let myRank: UserDTO
+    let rankList: [UserDTO]
 }
