a51e0673be5203417cf588f790408b3243f096f6
KimKiYoung <rlarldud6482@naver.com>
2022-01-17 22:11:00 +0900

    ⚡️::Domain UserRepository Protocol설정

---
+++ Service/Sources/Domain/Repository/UserRepository.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+import RxSwift
+
+protocol UserRepository {
+    func changePassword(accountID: String, phoneNumber: String, authCode: String, newPassword: String) -> Single<Void>
+    func profileInquiry(userID: Int) -> Single<UserProfileDTO>
+    func myPageInquiry() -> Single<UserProfileDTO>
+    func badgeInquiry(userID: Int) -> Single<BadgeListDTO>
+    func mainBadgeSet(badgeID: Int) -> Single<Void>
+    func changeProfile(name: String, profileImageUrlString: String, birthday: String, sex: String) -> Single<Void>
+    func findID(phoneNumber: String) -> Single<FindUserIdDTO>
+    func writeHealth(height: Float, weight: Int) -> Single<Void>
+    func joinClass(agencyCode: String, grade: Int, class: Int) -> Single<Void>
+}
