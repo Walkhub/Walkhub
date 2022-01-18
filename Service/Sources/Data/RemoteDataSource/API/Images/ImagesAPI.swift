870214d2c390be94b97350cf87175f785ae06b54
KimKiYoung <rlarldud6482@naver.com>
2022-01-18 11:55:27 +0900

    ⚡️::imagesAPI 설정

---
+++ Service/Sources/Data/RemoteDataSource/API/Images/ImagesAPI.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+import Moya
+
+enum ImagesAPI {
+    case postImages(images: [Data])
+}
+
+extension ImagesAPI: WalkhubAPI {
+    var domain: ApiDomain {
+        return .images
+    }
+    
+    var urlPath: String {
+        return "/"
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var task: Task {
+        switch self {
+        case .postImages(let images):
+            var multiformData = [MultipartFormData]()
+            for index in images {
+                multiformData.append(MultipartFormData(
+                    provider: .data(index),
+                    name: "images",
+                    fileName: "image.jpg",
+                    mimeType: "image/jpg"
+                ))
+            }
+            return .uploadMultipart(multiformData)
+        }
+    }
+    
+    var jwtTokenType: JWTTokenType? {
+        return .accessToken
+    }
+}
