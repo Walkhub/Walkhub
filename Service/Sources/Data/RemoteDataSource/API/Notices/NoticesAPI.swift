05710716c2230d95cd4535b0d0817b822d2c4af9
KimKiYoung <rlarldud6482@naver.com>
2022-01-18 09:36:38 +0900

    ⚡️::NoticesAPI 설정

---
+++ Service/Sources/Data/RemoteDataSource/API/Notices/NoticesAPI.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+import Moya
+
+enum NoticesAPI {
+    case seeNotice
+    case writeNotice(title: String, content: String, scope: String)
+    case deleteNotice(noticeID: Int)
+}
+
+extension NoticesAPI: WalkhubAPI {
+    var domain: ApiDomain {
+        .notices
+    }
+    
+    var urlPath: String {
+        switch self {
+        case .seeNotice:
+            return "/list"
+        case .deleteNotice(let noticeID):
+            return "/\(noticeID)"
+        default:
+            return "/"
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .writeNotice(let title, let content, let scope):
+            return .requestParameters(
+                parameters: [
+                    "title": title,
+                    "content": content,
+                    "scope": scope
+                ],
+                encoding: JSONEncoding.prettyPrinted
+            )
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .seeNotice:
+            return .get
+        case .deleteNotice:
+            return .delete
+        default:
+            return .post
+        }
+    }
+    
+    var jwtTokenType: JWTTokenType? {
+        return .accessToken
+    }
+    
+    
+}
