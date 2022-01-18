2433b415c6e4bff0d0318504635705a8dcd5d752
KimKiYoung <rlarldud6482@naver.com>
2022-01-18 12:53:48 +0900

    📑 ::Repository 삭제

--- Service/Sources/Domain/Repository/NoticesRepository.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-import RxSwift
-
-protocol NoticesRepository {
-    func seeNotice() -> Single<NoticeListDTO>
-    func writeNotice(title: String, content: String, scope: String) -> Single<Void>
-    func deleteNotice(noticeID: Int) -> Single<Void>
-}
