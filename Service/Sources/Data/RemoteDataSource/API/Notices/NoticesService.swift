45869bbab8daa33581b2a99bc6b545bbf9b90aff
KimKiYoung <rlarldud6482@naver.com>
2022-01-18 09:36:54 +0900

    ⚡️::NoticesService 설정

---
+++ Service/Sources/Data/RemoteDataSource/API/Notices/NoticesService.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+import Moya
+import RxSwift
+
+final class NoticesService: BaseService<NoticesAPI> {
+    
+    static let shared = NoticesService()
+    
+    private override init() {}
+    
+    func seeNotice() -> Single<Response> {
+        return request(.seeNotice)
+    }
+    
+    func writeNotice(
+        title: String,
+        content: String,
+        scope: String
+    ) -> Single<Response> {
+        return request(.writeNotice(
+            title: title,
+            content: content,
+            scope: scope))
+    }
+    
+    func deleteNotice(noticeID: Int) -> Single<Response> {
+        return request(.deleteNotice(noticeID: noticeID))
+    }
+}
