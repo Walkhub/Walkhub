cd5b238eeff95e558882eb265914c36259da6684
KimKiYoung <rlarldud6482@naver.com>
2022-01-18 09:07:17 +0900

    ⚡️::parameters&school API 추가

--- Service/Sources/Data/RemoteDataSource/API/User/UserAPI.swift
+++ Service/Sources/Data/RemoteDataSource/API/User/UserAPI.swift
@@ -11,7 +11,8 @@ enum UserAPI {
     case changeProfile(name: String, profileImageUrlString: String, birthday: String, sex: String)
     case findID(phoneNumber: String)
     case writeHealth(height: Float, weight: Int)
-    case joinClass(agencyCode: String, grade: Int, class: Int)
+    case joinClass(agencyCode: String, grade: Int, classNum: Int)
+    case changeSchoolInformation(agencyCode: String)
 }
 
 extension UserAPI: WalkhubAPI {
@@ -36,14 +37,57 @@ extension UserAPI: WalkhubAPI {
             return "/healths"
         case .joinClass(let code, let grade, let classNum):
             return "/classes/\(code)/\(grade)/\(classNum)"
+        case .changeSchoolInformation:
+            return "/school"
         default:
             return "/"
         }
     }
     
+    var task: Task {
+        switch self {
+        case .changePassword(let accountID, let phoneNumber, let authCode, let newPassword):
+            return .requestParameters(
+                parameters: [
+                    "account_id": accountID,
+                    "phone_number": phoneNumber,
+                    "auth_code": authCode,
+                    "new_password": newPassword
+                ],
+                encoding: JSONEncoding.prettyPrinted
+            )
+        case .changeProfile(let name, let profileImageUrlString, let birthday, let sex):
+            return .requestParameters(
+                parameters: [
+                    "name": name,
+                    "profile_image_url": profileImageUrlString,
+                    "birthday": birthday,
+                    "sex": sex
+                ],
+                encoding: JSONEncoding.prettyPrinted
+            )
+        case .writeHealth(let height, let weight):
+            return .requestParameters(
+                parameters: [
+                    "height": height,
+                    "weight": weight
+                ],
+                encoding: JSONEncoding.prettyPrinted
+            )
+        case .changeSchoolInformation(let agencyCode):
+            return .requestParameters(
+                parameters: [
+                    "agency_code": agencyCode
+                ],
+                encoding: JSONEncoding.prettyPrinted
+            )
+        default: return .requestPlain
+        }
+    }
+    
     var method: Moya.Method {
         switch self {
-        case .changeProfile, .changeProfile:
+        case .changeProfile, .changeProfile, .changeSchoolInformation, .writeHealth:
             return .patch
         case .badgeInquiry:
             return .put
@@ -62,5 +106,4 @@ extension UserAPI: WalkhubAPI {
             return .accessToken
         }
     }
-    
 }
