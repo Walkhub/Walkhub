dca394da502270d5b56dbb1bd77075602479606b
KimKiYoung <rlarldud6482@naver.com>
2022-01-19 10:06:39 +0900

    ⚡️::HealthKitDataSource 데이터 가져오는 함수 구현

---
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSoruce.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+import HealthKit
+
+class HealthKitDataSoruce {
+    let healthStore = HKHealthStore()
+    func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit,
+                   completion: @escaping (Double) -> Double) {
+        guard let sampleData = HKSampleType.quantityType(forIdentifier: dataType) else { fatalError() }
+        let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
+        let query = HKStatisticsQuery(quantityType: sampleData, quantitySamplePredicate: predicate,
+                                      options: [.cumulativeSum]) { _, result, error in
+            guard let quantity = result?.sumQuantity() else {
+                print(error?.localizedDescription)
+                completion(0.0)
+                return
+            }
+            DispatchQueue.main.async {
+                completion(quantity.doubleValue(for: unit))
+            }
+        }
+        healthStore.execute(query)
+    }
+}
+
