01d8e47f084487035bfc0be9bdba4bcc347d54ea
KimKiYoung <rlarldud6482@naver.com>
2022-01-19 10:16:01 +0900

    ⚡️ ::error optional 처리

--- Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSoruce.swift
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSoruce.swift
@@ -5,13 +5,13 @@ import HealthKit
 class HealthKitDataSoruce {
     let healthStore = HKHealthStore()
     func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit,
-                   completion: @escaping (Double) -> Double) {
+                   completion: @escaping (Double) -> Void) {
         guard let sampleData = HKSampleType.quantityType(forIdentifier: dataType) else { fatalError() }
         let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
         let query = HKStatisticsQuery(quantityType: sampleData, quantitySamplePredicate: predicate,
                                       options: [.cumulativeSum]) { _, result, error in
             guard let quantity = result?.sumQuantity() else {
-                print(error?.localizedDescription)
+                print(error?.localizedDescription ?? "")
                 completion(0.0)
                 return
             }
@@ -22,4 +22,3 @@ class HealthKitDataSoruce {
         healthStore.execute(query)
     }
 }
-
