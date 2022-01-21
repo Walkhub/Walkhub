73d0118fde09f16ce3df266b54a1056c62498c58
kimxwan <kimxwan0319@naver.com>
2022-01-21 09:09:29 +0900

    ♻️ :: BadgeDTO 분할

---
+++ Service/Sources/Data/RemoteDataSource/DataMapping/BadgeDTO.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct BadgeDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case imageUrlString = "image_url"
+    }
+    let id: Int
+    let name: String
+    let imageUrlString: String
+}
--- Service/Sources/Data/RemoteDataSource/DataMapping/BadgeListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/BadgeListDTO.swift
@@ -4,18 +4,5 @@ struct BadgeListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case badgeList = "badge_list"
     }
-    let badgeList: [BadgeInformation]
-}
-
-extension BadgeListDTO {
-    struct BadgeInformation: Decodable {
-        private enum CodingKeys: String, CodingKey {
-            case id
-            case name
-            case imageUrlString = "image_url"
-        }
-        let id: Int
-        let name: String
-        let imageUrlString: String
-    }
+    let badgeList: [BadgeDTO]
 }
