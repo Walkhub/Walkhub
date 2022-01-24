3381ac39c93e73351fe8a7b33c754fa2d192f02b
kimxwan <kimxwan0319@naver.com>
2022-01-25 01:19:20 +0900

    ⚡️ :: UserRankDTO/toDomain()

--- Service/Sources/Data/RemoteDataSource/DataMapping/UserRankDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/UserRankDTO.swift
@@ -9,3 +9,13 @@ struct UserRankDTO: Decodable {
     let myRank: UserDTO
     let rankList: [UserDTO]
 }
+
+// MARK: - Mappings to Domain
+extension UserRankDTO {
+    func toDomain() -> UserRank {
+        return .init(
+            myRank: myRank.toDomain(),
+            rankList: rankList.map { $0.toDomain() }
+        )
+    }
+}
