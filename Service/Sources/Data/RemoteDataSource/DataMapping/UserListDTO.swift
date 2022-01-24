2987cf29030574ba942882982168fcaeeb5c9487
kimxwan <kimxwan0319@naver.com>
2022-01-25 01:31:37 +0900

    ♻️ :: 프로퍼티명 수정

--- Service/Sources/Data/RemoteDataSource/DataMapping/UserListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/DataMapping/UserListDTO.swift
@@ -3,7 +3,7 @@ import Foundation
 // MARK: - Data Transfer Object
 struct UserListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case userList = "user_list"
+        case list = "user_list"
     }
     let userList: [UserDTO]
 }
