16fe30797c8c5eb312f0b39025556c65eefd265a
kimxwan <kimxwan0319@naver.com>
2022-01-25 01:35:14 +0900

    ⚡️ :: RankRepository

--- Service/Sources/Domain/Repository/RankRepository.swift
+++ Service/Sources/Domain/Repository/RankRepository.swift
@@ -1,1 +1,10 @@
+import Foundation
 
+import RxSwift
+
+protocol RankRepository {
+    func fetchSchoolRank(dateType: String) -> Observable<SchoolRank>
+    func searchSchool(name: String) -> Single<[School]>
+    func fetchUserRank(scope: String, dateTypa: String, sort: String, agencyCode: String) -> Observable<UserRank>
+    func searchUser(name: String, scope: String, agencyCode: String, grade: Int, classNum: Int) -> Single<[User]>
+}
