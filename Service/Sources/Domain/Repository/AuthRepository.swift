d793e656fb2ccda54aba06e7cccb1029187deff2
KimKiYoung <rlarldud6482@naver.com>
2022-01-26 11:46:21 +0900

    ⚡️:: AuthAPI & AuthRepository Error 해결

--- Service/Sources/Domain/Repository/AuthRepository.swift
+++ Service/Sources/Domain/Repository/AuthRepository.swift
@@ -1,9 +1,11 @@
+// swiftlint:disable function_parameter_count
 import Foundation
 
 import RxSwift
 
 protocol AuthRepository {
     func signin(id: String, password: String) -> Single<Void>
-    func signup(id: String, password: String, name: String, phoneNumber: String, authCode: String) -> Single<Void>
+    func signup(id: String, password: String, name: String, phoneNumber: String, authCode: String, height: Float,
+                weight: Int, birthday: String, sex: String, agencyCode: String) -> Single<Void>
     func verificationPhone(phoneNumber: String) -> Single<Void>
 }
