aff75b0f4bd265a56e4e55b775d53b9abea6d16d
kimxwan <kimxwan0319@naver.com>
2022-01-27 03:20:28 +0900

    ⚡️ :: RankRepository 프로토콜 수정

--- Service/Sources/Domain/Repository/RankRepository.swift
+++ Service/Sources/Domain/Repository/RankRepository.swift
@@ -3,8 +3,9 @@ import Foundation
 import RxSwift
 
 protocol RankRepository {
-    func fetchSchoolRank(dateType: String) -> Observable<SchoolRank>
+    func fetchSchoolRank(dateType: DateType) -> Observable<SchoolRank>
     func searchSchool(name: String) -> Single<[School]>
-    func fetchUserRank(scope: String, dateTypa: String, sort: String, agencyCode: String) -> Observable<UserRank>
-    func searchUser(name: String, scope: String, agencyCode: String, grade: Int, classNum: Int) -> Single<[User]>
+    func fetchUserSchoolRank(scope: Scope, dateType: DateType) -> Observable<UserRank>
+    func fetchUserRank(scope: Scope, dateTypa: DateType, agencyCode: String) -> Single<[User]>
+    func searchUser(name: String, scope: Scope, agencyCode: String, grade: Int, classNum: Int) -> Single<[User]>
 }
