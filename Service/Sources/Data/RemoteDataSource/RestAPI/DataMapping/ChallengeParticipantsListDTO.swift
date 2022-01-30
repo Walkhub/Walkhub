4934e9e1501ffe2e49389d51491b2d27648b0aed
kimxwan <kimxwan0319@naver.com>
2022-01-31 00:52:01 +0900

    ♻️ :: DTO 네이밍 변경

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeParticipantsListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeParticipantsListDTO.swift
@@ -3,14 +3,14 @@
 import Foundation
 
 // MARK: - Data Transfer Object
-struct ParticipantsListDTO: Decodable {
+struct ChallengeParticipantsListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case list = "challenge_participants_list"
     }
     let list: [Participants]
 }
 
-extension ParticipantsListDTO {
+extension ChallengeParticipantsListDTO {
     struct Participants: Decodable {
         private enum CodingKeys: String, CodingKey {
             case id
