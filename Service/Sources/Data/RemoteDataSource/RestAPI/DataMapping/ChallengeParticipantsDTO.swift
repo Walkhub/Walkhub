93b33a6ebdf423e9239d15cb31cfda37777e6749
kimxwan <kimxwan0319@naver.com>
2022-01-31 00:53:31 +0900

    ⚡️ :: ChallengeParticipantsDTO 분할

---
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeParticipantsDTO.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// MARK: - Data Transfer Object
+struct ChallengeParticipantsDTO: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case gcn
+        case profileImageUrlString = "profile_image_url"
+    }
+    let id: Int
+    let name: String
+    let gcn: String
+    let profileImageUrlString: String
+}
--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeParticipantsListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeParticipantsListDTO.swift
@@ -1,26 +1,8 @@
-// swiftlint:disable nesting
-
 import Foundation
 
-// MARK: - Data Transfer Object
 struct ChallengeParticipantsListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case list = "challenge_participants_list"
     }
-    let list: [Participants]
-}
-
-extension ChallengeParticipantsListDTO {
-    struct Participants: Decodable {
-        private enum CodingKeys: String, CodingKey {
-            case id
-            case name
-            case gcn
-            case profileImageUrlString = "profile_image_url"
-        }
-        let id: Int
-        let name: String
-        let gcn: String
-        let profileImageUrlString: String
-    }
+    let list: [ChallengeParticipantsDTO]
 }
