bab500d45014801c76964dcf431fe4b40d35aa7f
kimxwan <kimxwan0319@naver.com>
2022-01-31 01:03:46 +0900

    ♻️ :: NoticeListDTO 프로퍼티 네이밍 수정

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/NoticeListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/NoticeListDTO.swift
@@ -3,14 +3,14 @@ import Foundation
 // MARK: - Data Transfer Object
 struct NoticeListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case noticeList = "notice_list"
+        case list = "notice_list"
     }
-    let noticeList: [NoticeDTO]
+    let list: [NoticeDTO]
 }
 
 // MARK: - Mappings to Domain
 extension NoticeListDTO {
     func toDomain() -> [Notice] {
-        return noticeList.map { $0.toDomain() }
+        return list.map { $0.toDomain() }
     }
 }
