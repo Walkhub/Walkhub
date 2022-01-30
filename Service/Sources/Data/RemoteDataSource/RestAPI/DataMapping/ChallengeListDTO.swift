9a03d73e8850d9369407e40d31278da5e5218dce
kimxwan <kimxwan0319@naver.com>
2022-01-31 01:03:08 +0900

    ♻️ :: ChallengeListDTO 프로퍼티 네이밍 수정

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeListDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/ChallengeListDTO.swift
@@ -3,14 +3,14 @@ import Foundation
 // MARK: - Data Transfer Object
 struct ChallengeListDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case challengeList = "challenge_list"
+        case list = "challenge_list"
     }
-    let challengeList: [ChallengeDTO]
+    let list: [ChallengeDTO]
 }
 
 // MARK: - Mappings to Domain
 extension ChallengeListDTO {
     func toDomain() -> [Challenge] {
-        return challengeList.map { $0.toDomain() }
+        return list.map { $0.toDomain() }
     }
 }
