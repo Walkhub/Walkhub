2176938244cc79377363eaf8c10c12281915edb9
kimxwan <kimxwan0319@naver.com>
2022-01-31 01:01:02 +0900

    ♻️ :: DTO 네이밍 변경

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/UserIdDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/UserIdDTO.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // MARK: - Data Transfer Object
-struct FindUserIdDTO: Decodable {
+struct UserIdDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case userID = "account_id"
     }
