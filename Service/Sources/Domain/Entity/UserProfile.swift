ef66c023d30f830cda2b5cdf4edff9be8f801474
kimxwan <kimxwan0319@naver.com>
2022-01-31 20:48:00 +0900

    🐛 :: UserProfile entity 수정

--- Service/Sources/Domain/Entity/UserProfile.swift
+++ Service/Sources/Domain/Entity/UserProfile.swift
@@ -2,9 +2,9 @@ import Foundation
 
 struct UserProfile {
     let name: String
-    let profileImageUrlString: String?
+    let profileImageUrl: URL?
     let school: String?
     let grade: Int?
     let classNum: Int?
-    let titleBadge: BadgeDTO
+    let titleBadge: Badge
 }
