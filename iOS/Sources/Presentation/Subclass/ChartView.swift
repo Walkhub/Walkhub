4c3c3f1f827f5196f4f572e53cd8ff3f9cf6afbe
rlarldud1234 <rlarldud6482@naver.com>
2022-02-04 23:57:23 +0900

    ⚡️:: 랜덤 컬러 및 월간 차트 & 주간 차트 메소드 따로 생성

--- iOS/Sources/Presentation/Subclass/ChartView.swift
+++ iOS/Sources/Presentation/Subclass/ChartView.swift
@@ -10,9 +10,10 @@ class ChartView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         addSubviews()
+        setCharts()
     }
 
-    public func setCharts(
+    public func setWeekCharts(
         days: [String],
         stepCounts: [Int],
         chartColor: [UIColor]
@@ -33,20 +34,49 @@ class ChartView: UIView {
 
         chartDataSet.highlightEnabled = false
 
-        barChartView.doubleTapToZoomEnabled = false
-
-        barChartView.xAxis.labelPosition = .bottom
         barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: days)
-        barChartView.xAxis.drawAxisLineEnabled = false
-        barChartView.xAxis.drawGridLinesEnabled = false
+        barChartView.xAxis.setLabelCount(days.count, force: true)
 
         barChartView.leftAxis.axisMinimum = 0
         barChartView.leftAxis.granularity = 5000
+    }
 
-        barChartView.rightAxis.enabled = false
-        barChartView.rightAxis.setLabelCount(days.count, force: false)
+    public func setMothCharts(
+        days: [String],
+        stepCounts: [Int]
+    ) {
+        let color1 = UIColor.random()
+        let color2 = UIColor.random()
+        let color3 = UIColor.random()
+        let color4 = UIColor.random()
 
-        barChartView.legend.enabled = false
+        let chartColor = [color1, color1, color1, color1, color1, color1, color1,
+                          color2, color2, color2, color2, color2, color2, color2,
+                          color3, color3, color3, color3, color3, color3, color3,
+                          color4, color4, color4, color4, color4, color4, color4]
+
+        var dataEntries: [BarChartDataEntry] = []
+        for data in 0..<days.count {
+            let dataEntry = BarChartDataEntry(x: Double(data), y: Double(stepCounts[data]))
+            dataEntries.append(dataEntry)
+        }
+
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "걸음 수")
+        chartDataSet.colors = chartColor
+
+        let chartData = BarChartData(dataSet: chartDataSet)
+        chartData.setDrawValues(false)
+        chartData.barWidth = 0.4
+
+        barChartView.data = chartData
+
+        chartDataSet.highlightEnabled = false
+
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: days)
+        barChartView.xAxis.setLabelCount(days.count, force: false)
+
+        barChartView.leftAxis.axisMinimum = 0
+        barChartView.leftAxis.granularity = 5000
     }
 }
 
@@ -57,5 +87,15 @@ extension ChartView {
         barChartView.snp.makeConstraints {
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
+    }
+
+    private func setCharts() {
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.xAxis.drawAxisLineEnabled = false
+        barChartView.xAxis.drawGridLinesEnabled = false
+        barChartView.doubleTapToZoomEnabled = false
+        barChartView.rightAxis.enabled = false
+
+        barChartView.legend.enabled = false
     }
 }
