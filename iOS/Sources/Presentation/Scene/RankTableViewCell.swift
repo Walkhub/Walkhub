730249f5ddaefea90d9cc24393b73cb6058be710
sian7563 <sian7563@naver.com>
2022-02-06 12:32:14 +0900

    ⚡️ :: RankTableViewCell UI

---
+++ iOS/Sources/Presentation/Scene/RankTableViewCell.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+class RankTableViewCell: UITableViewCell {
+
+    let rankImageView = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 15
+        $0.image = .init(systemName: "circle.fill")
+        $0.tintColor = .init(named: "F9F9F9")!
+    }
+    
+    let rankNameLabel = UILabel().then {
+        $0.text = "정 환"
+        $0.font = .notoSansFont(ofSize: 16, family: .medium)
+    }
+    
+    let rankStepLabel = UILabel().then {
+        $0.text = "7482 걸음"
+        $0.font = .notoSansFont(ofSize: 12, family: .regular)
+    }
+    
+    let rankLabel = UILabel().then {
+        $0.text = "1등"
+        $0.font = .notoSansFont(ofSize: 16, family: .medium)
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUp()
+    }
+    
+    private func setUp() {
+        self.addSubview(rankImageView)
+        self.addSubview(rankNameLabel)
+        self.addSubview(rankStepLabel)
+        self.addSubview(rankLabel)
+        
+        rankImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(20)
+            $0.height.width.equalTo(40)
+        }
+        
+        rankNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(19)
+            $0.leading.equalTo(rankImageView.snp.trailing).offset(18)
+        }
+        
+        rankStepLabel.snp.makeConstraints {
+            $0.top.equalTo(rankNameLabel.snp.bottom)
+            $0.leading.equalTo(rankImageView.snp.trailing).offset(18)
+        }
+        
+        rankLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(28)
+            $0.trailing.equalToSuperview().inset(18)
+            $0.bottom.equalToSuperview().inset(12)
+        }
+    }
+}
