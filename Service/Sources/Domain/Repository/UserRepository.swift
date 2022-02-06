67bd722e45faba62adebceb9cd9866b37c4654e0
kimxwan <kimxwan0319@naver.com>
2022-02-07 08:53:43 +0900

    ⚡️ :: UserRepository 프로토콜 작성

---
+++ Service/Sources/Domain/Repository/UserRepository.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+import RxSwift
+
+protocol UserRepository {
+    func changePassword(accountID: String, phoneNumber: String, authCode: String, newPassword: String) -> Single<Void>
+    func fetchProfile(userID: Int) -> Observable<UserProfile>
+    func fetchMyInformation() -> Observable<UserProfile>
+    func fetchBadges(userID: Int) -> Observable<[Badge]>
+    func setMainBadge(badgeID: Int) -> Single<Void>
+    func changeProfile(name: String, profileImageUrlString: String, birthday: String, sex: Sex) -> Single<Void>
+    func writeHealth(height: Float, weight: Int) -> Single<Void>
+    func joinClass(agencyCode: String, grade: Int, classNum: Int) -> Single<Void>
+    func setSchoolInformation(agencyCode: String) -> Single<Void>
+}
