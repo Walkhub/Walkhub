4f7f76db06748ae3614414649e933d1fc040d3df
sian7563 <sian7563@naver.com>
2022-02-07 08:37:24 +0900

    ⚡️ :: extension 코드 수정

--- iOS/Sources/Presentation/Extension/ColorExtension.swift
+++ iOS/Sources/Presentation/Extension/ColorExtension.swift
@@ -1,16 +1,15 @@
-import Foundation
-import SwiftUI
+import UIKit
 
-extension Color {
-    static let gray50 = Color("Gray50")
-    static let gray100 = Color("Gray100")
-    static let gray200 = Color("Gray200")
-    static let gray300 = Color("Gray300")
-    static let gray400 = Color("Gray400")
-    static let gray500 = Color("Gray500")
-    static let gray600 = Color("Gray600")
-    static let gray700 = Color("Gray700")
-    static let gray800 = Color("Gray800")
-    static let gray900 = Color("Gray900")
+extension UIColor {
+    static let gray50 = UIColor(named: "Gray50")!
+    static let gray100 = UIColor(named: "Gray100")!
+    static let gray200 = UIColor(named: "Gray200")!
+    static let gray300 = UIColor(named: "Gray300")!
+    static let gray400 = UIColor(named: "Gray400")!
+    static let gray500 = UIColor(named: "Gray500")!
+    static let gray600 = UIColor(named: "Gray600")!
+    static let gray700 = UIColor(named: "Gray700")!
+    static let gray800 = UIColor(named: "Gray800")!
+    static let gray900 = UIColor(named: "Gray900")!
     static let primary400 = UIColor(named: "Primary400")!
 }
