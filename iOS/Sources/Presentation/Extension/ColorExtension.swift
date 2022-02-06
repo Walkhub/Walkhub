c7f210a03b0f3b1128116f9127f1e169a690f63c
sian7563 <sian7563@naver.com>
2022-02-06 20:09:41 +0900

    ⚡️ :: Add Color Asset

---
+++ iOS/Sources/Presentation/Extension/ColorExtension.swift
@@ -0,0 +1,24 @@
+//
+//  ColorExtension.swift
+//  Walkhub
+//
+//  Created by kimsian on 2022/02/06.
+//  Copyright © 2022 com.walkhub. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let gray50 = Color("Gray50")
+    static let gray100 = Color("Gray100")
+    static let gray200 = Color("Gray200")
+    static let gray300 = Color("Gray300")
+    static let gray400 = Color("Gray400")
+    static let gray500 = Color("Gray500")
+    static let gray600 = Color("Gray600")
+    static let gray700 = Color("Gray700")
+    static let gray800 = Color("Gray800")
+    static let gray900 = Color("Gray900")
+    static let primary400 = Color("Primary400")
+}
