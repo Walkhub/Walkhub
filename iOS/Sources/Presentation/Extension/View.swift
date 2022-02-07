78fc8ebf0f757b9e8428d7cc4ac3667f4e74724d
rlarldud1234 <rlarldud6482@naver.com>
2022-02-07 23:15:27 +0900

    ⚡️ :: View Extension

---
+++ iOS/Sources/Presentation/Extension/View.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIView {
+    func roundCorners(cornerRadius: CGFloat, byRoundingCorners: UIRectCorner) {
+        let path = UIBezierPath(roundedRect: self.bounds,
+                                byRoundingCorners: byRoundingCorners,
+                                cornerRadii: CGSize(width:cornerRadius, height: cornerRadius))
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = path.cgPath
+        
+        layer.mask = maskLayer
+    }
+}
