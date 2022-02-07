c3219b26b9c140809521605668ff78ea22b92420
kimxwan <kimxwan0319@naver.com>
2022-02-07 23:37:06 +0900

    ⚡️ :: Realm result가 비었으면 에러 방출

--- Service/Sources/Data/LocalDataSource/Realm/RealmTask.swift
+++ Service/Sources/Data/LocalDataSource/Realm/RealmTask.swift
@@ -65,13 +65,20 @@ public final class RealmTask: RealmTaskType {
         ordering: OrderingType = .ascending
     ) -> Single<[T]> {
         return Single<[T]>.create { single in
+
             let resultArr = self.fetchObjectsResults(
                 for: T.self,
                 filter: filter,
                 sortProperty: sortProperty,
                 ordering: ordering
             ).toArray()
-            single(.success(resultArr))
+
+            if resultArr.isEmpty {
+                single(.failure(RealmError.noData))
+            } else {
+                single(.success(resultArr))
+            }
+
             return Disposables.create()
         }
     }
