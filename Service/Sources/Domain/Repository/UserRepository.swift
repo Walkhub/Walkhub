1b04b9afa01f4dec863935e25a189aac9a2b9264
kimxwan <kimxwan0319@naver.com>
2022-02-07 09:14:55 +0900

    ♻️ :: 네이밍 수정 myInformation -> myProfile

--- Service/Sources/Domain/Repository/UserRepository.swift
+++ Service/Sources/Domain/Repository/UserRepository.swift
@@ -5,7 +5,7 @@ import RxSwift
 protocol UserRepository {
     func changePassword(accountID: String, phoneNumber: String, authCode: String, newPassword: String) -> Single<Void>
     func fetchProfile(userID: Int) -> Observable<UserProfile>
-    func fetchMyInformation() -> Observable<UserProfile>
+    func fetchMyProfile() -> Observable<UserProfile>
     func fetchBadges(userID: Int) -> Observable<[Badge]>
     func setMainBadge(badgeID: Int) -> Single<Void>
     func changeProfile(name: String, profileImageUrlString: String, birthday: String, sex: Sex) -> Single<Void>
