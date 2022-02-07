200f4453f0028afc17bf753ba916eaf253404843
kimxwan <kimxwan0319@naver.com>
2022-02-07 15:47:55 +0900

    ⚡️ :: not null 값 옵셔널 해제

--- Service/Sources/Domain/Entity/UserProfile.swift
+++ Service/Sources/Domain/Entity/UserProfile.swift
@@ -3,8 +3,8 @@ import Foundation
 struct UserProfile {
     let userID: Int
     let name: String
-    let profileImageUrl: URL?
-    let school: String?
+    let profileImageUrl: URL
+    let school: String
     let grade: Int?
     let classNum: Int?
     let titleBadge: Badge
