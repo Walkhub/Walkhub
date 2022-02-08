4594bafc48d21c82e4b58973c575a3fb006528d7
kimxwan <kimxwan0319@naver.com>
2022-02-08 15:17:29 +0900

    ♻️ :: 접근 제어 지시자 범위에 맞게 수정

--- Service/Sources/Data/LocalDataSource/HealthKit/HealthKitTask.swift
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitTask.swift
@@ -3,9 +3,9 @@ import Foundation
 import HealthKit
 import RxSwift
 
-final public class HealthKitTask {
+final class HealthKitTask {
 
-    public static let shared = HealthKitTask()
+    static let shared = HealthKitTask()
 
     let healthStore = HKHealthStore()
     let healthKitTypes: Set = [
@@ -15,7 +15,7 @@ final public class HealthKitTask {
 
     private init() { }
 
-    public func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit) -> Single<Double> {
+    func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit) -> Single<Double> {
         return Single<Double>.create { single in
             guard let sampleData = HKSampleType.quantityType(forIdentifier: dataType) else { fatalError() }
             let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
@@ -35,7 +35,7 @@ final public class HealthKitTask {
         }
     }
 
-    public func setHealthKit() {
+    func setHealthKit() {
         healthStore.requestAuthorization(toShare: nil, read: healthKitTypes) { _, _ in }
     }
 }
--- Service/Sources/Data/LocalDataSource/Realm/Extension/Realm+Util.swift
+++ Service/Sources/Data/LocalDataSource/Realm/Extension/Realm+Util.swift
@@ -4,11 +4,11 @@ import RealmSwift
 
 extension Results {
 
-    public func toArray() -> [Element] {
+    func toArray() -> [Element] {
         return Array(self)
     }
 
-    public func toArray<T>(type: T.Type) -> [T] {
+    func toArray<T>(type: T.Type) -> [T] {
         return compactMap { $0 as? T }
     }
 
