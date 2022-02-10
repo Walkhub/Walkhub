3231d140dd1db982e3b608b2a9e3d2b67309f45a
sian7563 <sian7563@naver.com>
2022-02-10 15:58:52 +0900

    ⚡️ :: Edit SecondMainPageTableViewCell

--- iOS/Sources/Presentation/Scene/SecondMainpageTableViewCell.swift
+++ iOS/Sources/Presentation/Scene/SecondMainpageTableViewCell.swift
@@ -4,12 +4,14 @@ class SecondMainpageTableViewCell: UITableViewCell {
 
     let fastLabel = UILabel().then {
         $0.font = .notoSansFont(ofSize: 16, family: .medium)
+        $0.text = "누구보다 빠르게"
     }
-    
+
     let recordLabel = UILabel().then {
         $0.font = .notoSansFont(ofSize: 16, family: .bold)
+        $0.text = "지금 바로 기록하기"
     }
-    
+
     let rankImageView = UIImageView().then {
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 15
@@ -21,41 +23,38 @@ class SecondMainpageTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setUp()
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        demoData()
+        if selected {
+            self.setSelected(false, animated: true)
+        }
     }
-    
-    private func demoData() {
-        fastLabel.text = "누구보다 빠르게"
-        recordLabel.text = "지금 바로 기록하기"
-    }
-    
+
     private func setUp() {
-        self.addSubview(fastLabel)
-        self.addSubview(recordLabel)
-        self.addSubview(rankImageView)
-        
+        self.contentView.addSubview(fastLabel)
+        self.contentView.addSubview(recordLabel)
+        self.contentView.addSubview(rankImageView)
+
         fastLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(24)
             $0.leading.equalToSuperview().inset(24)
         }
-        
+
         recordLabel.snp.makeConstraints {
             $0.top.equalTo(fastLabel.snp.bottom)
             $0.leading.equalToSuperview().inset(24)
         }
-        
+
         rankImageView.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().inset(24)
