dc3bca786df07b49cc60d3eb61cfb0bb0206cc97
kimxwan <kimxwan0319@naver.com>
2022-02-11 22:43:40 +0900

    ⚡️ :: HealthKitDataSource

--- Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSource.swift
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSource.swift
@@ -1,34 +1,52 @@
 import Foundation
 
+import HealthKit
 import RxSwift
 
 final class HealthKitDataSource {
 
     static let shared = HealthKitDataSource()
 
+    private let disposeBag = DisposeBag()
+
     private init() { }
 
-    func stepCount(
+    func observeStepCountDataSignal() -> Observable<Void> {
+        return HealthKitTask.shared.observingDataChange(dataType: .stepCount)
+    }
+
+    func fetchStepCount(
         start: Date,
         end: Date
-    ) ->  Single<Double> {
-        return HealthKitTask.shared.fetchData(
+    ) ->  Single<Int> {
+        return HealthKitTask.shared.fetchDataValue(
             start: start,
             end: end,
             dataType: .stepCount,
             unit: .count()
-        )
+        ).map { Int($0) }
     }
 
-    func walkDistance(
+    func fetchWalkDistance(
         start: Date,
         end: Date
     ) -> Single<Double> {
-        return HealthKitTask.shared.fetchData(
+        return HealthKitTask.shared.fetchDataValue(
             start: start,
             end: end,
             dataType: .distanceWalkingRunning,
             unit: .meter()
+        )
+    }
+
+    func fetchWalkData(
+        start: Date,
+        end: Date
+    ) -> Single<[HKSample]> {
+        return HealthKitTask.shared.fetchData(
+            start: start,
+            end: end,
+            dataType: .stepCount
         )
     }
 
