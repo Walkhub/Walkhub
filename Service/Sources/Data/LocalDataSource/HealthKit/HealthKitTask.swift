eb0e3e5ddf055934e97aa5604687d4a2e47eee7d
kimxwan <kimxwan0319@naver.com>
2022-02-11 22:28:46 +0900

    ♻️ :: HealthKitTask 리펙토링, 필요 메소드 추가

--- Service/Sources/Data/LocalDataSource/HealthKit/HealthKitTask.swift
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitTask.swift
@@ -15,27 +15,86 @@ final class HealthKitTask {
 
     private init() { }
 
-    func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit) -> Single<Double> {
+    func fetchData(start: Date, end: Date, dataType: HKQuantityTypeIdentifier) -> Single<[HKSample]> {
+        return Single<[HKSample]>.create { single in
+            let authorization = self.requestAuthorization()
+                .filter { $0 }
+                .subscribe(onSuccess: { _ in
+                    let sampleType = HKSampleType.quantityType(forIdentifier: dataType)!
+                    let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
+                    let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+                    let query = HKSampleQuery(
+                        sampleType: sampleType,
+                        predicate: predicate,
+                        limit: HKObjectQueryNoLimit,
+                        sortDescriptors: [sortDescriptor]
+                    ) { _, result, error in
+                        guard let result = result else {
+                            single(.failure(error!))
+                            return
+                        }
+                        single(.success(result))
+                    }
+
+                    self.healthStore.execute(query)
+                })
+            return Disposables.create([authorization])
+        }
+    }
+
+    func fetchDataValue(start: Date, end: Date, dataType: HKQuantityTypeIdentifier, unit: HKUnit) -> Single<Double> {
         return Single<Double>.create { single in
-            guard let sampleData = HKSampleType.quantityType(forIdentifier: dataType) else { fatalError() }
-            let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
-            let query = HKStatisticsQuery(
-                quantityType: sampleData,
-                quantitySamplePredicate: predicate,
-                options: [.cumulativeSum]
-            ) { _, result, error in
-                guard let quantity = result?.sumQuantity() else {
-                    single(.failure(error!))
-                    return
-                }
-                single(.success(quantity.doubleValue(for: unit)))
+            let authorization = self.requestAuthorization()
+                .filter { $0 }
+                .subscribe(onSuccess: { _ in
+                    let sampleType = HKSampleType.quantityType(forIdentifier: dataType)!
+                    let predicate = HKQuery.predicateForSamples(withStart: start, end: end, options: .strictStartDate)
+                    let query = HKStatisticsQuery(
+                        quantityType: sampleType,
+                        quantitySamplePredicate: predicate,
+                        options: [.cumulativeSum]
+                    ) { _, result, error in
+                        guard let quantity = result?.sumQuantity() else {
+                            single(.failure(error!))
+                            return
+                        }
+                        single(.success(quantity.doubleValue(for: unit)))
+                    }
+
+                    self.healthStore.execute(query)
+                })
+            return Disposables.create([authorization])
+        }
+    }
+
+    func observingDataSignal(dataType: HKQuantityTypeIdentifier) -> Observable<Void> {
+        return Observable<Void>.create { observer in
+            let authorization = self.requestAuthorization()
+                .filter { $0 }
+                .subscribe(onSuccess: { _ in
+                    let sampleType = HKSampleType.quantityType(forIdentifier: dataType)!
+
+                    let query = HKObserverQuery(sampleType: sampleType, predicate: nil) { _, _, error in
+                        if let error = error {
+                            observer.onError(error)
+                            return
+                        }
+                        observer.onNext(())
+                    }
+
+                    self.healthStore.execute(query)
+                })
+            return Disposables.create([authorization])
+        }
+    }
+
+    private func requestAuthorization() -> Single<Bool> {
+        return Single<Bool>.create { single in
+            self.healthStore.requestAuthorization(toShare: nil, read: self.healthKitTypes) { isAllowed, _ in
+                single(.success(isAllowed))
             }
-            self.healthStore.execute(query)
             return Disposables.create()
         }
     }
 
-    func setHealthKit() {
-        healthStore.requestAuthorization(toShare: nil, read: healthKitTypes) { _, _ in }
-    }
 }
