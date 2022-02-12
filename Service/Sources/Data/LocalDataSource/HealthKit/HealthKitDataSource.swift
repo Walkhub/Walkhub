d760c0d5bd5248f412560ac3e3e45bc34b98fc20
kimxwan <kimxwan0319@naver.com>
2022-02-13 01:37:35 +0900

    ⚡️ :: HealthKitDataSource에 키/몸무게 읽기/쓰기 추가

--- Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSource.swift
+++ Service/Sources/Data/LocalDataSource/HealthKit/HealthKitDataSource.swift
@@ -7,19 +7,20 @@ final class HealthKitDataSource {
 
     static let shared = HealthKitDataSource()
 
+    private let healthKitTask = HealthKitTask.shared
     private let disposeBag = DisposeBag()
 
     private init() { }
 
     func observeStepCountDataSignal() -> Observable<Void> {
-        return HealthKitTask.shared.observingDataChange(dataType: .stepCount)
+        return healthKitTask.observingDataChange(dataType: .stepCount)
     }
 
     func fetchStepCount(
         start: Date,
         end: Date
     ) ->  Single<Int> {
-        return HealthKitTask.shared.fetchDataValue(
+        return healthKitTask.fetchDataValue(
             start: start,
             end: end,
             dataType: .stepCount,
@@ -31,7 +32,7 @@ final class HealthKitDataSource {
         start: Date,
         end: Date
     ) -> Single<Double> {
-        return HealthKitTask.shared.fetchDataValue(
+        return healthKitTask.fetchDataValue(
             start: start,
             end: end,
             dataType: .distanceWalkingRunning,
@@ -43,10 +44,42 @@ final class HealthKitDataSource {
         start: Date,
         end: Date
     ) -> Single<[HKSample]> {
-        return HealthKitTask.shared.fetchData(
+        return healthKitTask.fetchData(
             start: start,
             end: end,
             dataType: .stepCount
+        )
+    }
+
+    func fetchUserHeight() -> Single<Double> {
+        return healthKitTask.fetchData(
+            dataCountLimit: 1,
+            dataType: .height
+        ).map { $0.first?.autoContentAccessingProxy as? Double ?? 0 }
+    }
+
+    func fetchUserWeight() -> Single<Double> {
+        return healthKitTask.fetchData(
+            dataCountLimit: 1,
+            dataType: .bodyMass
+        ).map { $0.first?.autoContentAccessingProxy as? Double ?? 0 }
+    }
+
+    func storeUserHeight(_ height: Double) {
+        healthKitTask.storeData(
+            dataValue: height,
+            unit: .meterUnit(with: .centi),
+            date: Date(),
+            dataType: .height
+        )
+    }
+
+    func storeUserWeight(_ weight: Double) {
+        healthKitTask.storeData(
+            dataValue: weight,
+            unit: .gramUnit(with: .kilo),
+            date: Date(),
+            dataType: .height
         )
     }
 
