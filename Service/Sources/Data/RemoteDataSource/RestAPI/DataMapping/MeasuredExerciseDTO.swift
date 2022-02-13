d768420a637177760965471701ed6f908fae8261
kimxwan <kimxwan0319@naver.com>
2022-02-14 03:34:36 +0900

    ⚡️ :: MeasuredExerciseDTO

---
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/MeasuredExerciseDTO.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// MARK: - Data Transfer Object
+struct MeasuredExerciseDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case exerciseId = "exercise_list"
+        case imageUrlString = "image_url"
+        case startAt = "start_at"
+        case latitude
+        case longitude
+    }
+    let exerciseId: Int
+    let imageUrlString: String
+    let startAt: String
+    let latitude: Double
+    let longitude: Double
+}
+
+// MARK: - Mappings to Domain
+extension MeasuredExerciseDTO {
+    func toDomain() -> MeasuredExercise {
+        return .init(
+            exerciseId: exerciseId,
+            imageUrl: URL(string: imageUrlString)!,
+            startAt: startAt.toDate(),
+            latitude: latitude,
+            longitude: longitude
+        )
+    }
+}
