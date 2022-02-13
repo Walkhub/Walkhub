5912e6d49252c4fae6f999a232ab3dd59e8d33b1
kimxwan <kimxwan0319@naver.com>
2022-02-13 18:52:44 +0900

    ⚡️ :: MeasuringExerciseRecord 작성

---
+++ Service/Sources/Domain/Entity/MeasuringExerciseRecord.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct MeasuringExerciseRecord {
+    let stepCount: Int
+    let wlkingRunningTimeAsSecond: Double
+    let walkingRunningDistanceAsMeter: Double
+    let speedAsMeterPerSecond: Double
+    let burnedKilocalories: Double
+}
