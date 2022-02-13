98e26452646f072c963a61a22676ab3eb2ae9f80
kimxwan <kimxwan0319@naver.com>
2022-02-14 04:01:22 +0900

    ⚡️ :: ExerciseAnalysis 작성

---
+++ Service/Sources/Domain/Entity/ExerciseAnalysis.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct ExerciseAnalysis: Equatable {
+    public let walkCountList: [Int]
+    public let dailyWalkCountGoal: Int
+    public let walkCount: Int
+    public let calorie: Double
+    public let distane: Double
+    public let walkTime: Double
+}
