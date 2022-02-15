1a2a69f566566e4d2ecc30086ca64fc503e48acf
rlarldud1234 <rlarldud6482@naver.com>
2022-02-15 11:38:10 +0900

    ⚡️ :: 파일 그룹명 Calories -> CaloriesLevel

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/Calories/CaloriesLevelListDTO.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct CaloriesLevelListDTO: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case caloriesLevelList = "calories_level_list"
-    }
-    let caloriesLevelList: [CaloriesLevelDTO]
-}
