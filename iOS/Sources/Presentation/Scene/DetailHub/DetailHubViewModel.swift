ce30ff19a34e386e45295f0336a4c486125ba8a7
rlarldud1234 <rlarldud6482@naver.com>
2022-02-16 12:16:41 +0900

    ⚡️ :: DetailHubViewModel 생성자 및 input & output 변경

--- iOS/Sources/Presentation/Scene/DetailHub/DetailHubViewModel.swift
+++ iOS/Sources/Presentation/Scene/DetailHub/DetailHubViewModel.swift
@@ -7,10 +7,17 @@ import Service
 class DetailHubViewModel: ViewModelType {
 
     private let searchUserUseCase: SearchUserUseCase
+    private let fetchUserSchoolRankUseCase: FetchUserSchoolRankUseCase
+    private let fetchUserRankUseCase: FetchUserRankUseCase
 
-    init(searchUserUseCase: SearchUserUseCase) {
-        self.searchUserUseCase = searchUserUseCase
-    }
+    init(
+        searchUserUseCase: SearchUserUseCase,
+        fetchUserSchoolRankUseCase: FetchUserSchoolRankUseCase,
+        fetchUserRankUseCase: FetchUserRankUseCase) {
+            self.searchUserUseCase = searchUserUseCase
+            self.fetchUserSchoolRankUseCase = fetchUserSchoolRankUseCase
+            self.fetchUserRankUseCase = fetchUserRankUseCase
+        }
 
     private var disposeBag = DisposeBag()
 
@@ -18,22 +25,59 @@ class DetailHubViewModel: ViewModelType {
         let name: Driver<String>
         let schoolId: Driver<Int>
         let dateType: Driver<DateType>
+        let switchOn: Driver<Scope>
+        let isMySchool: Driver<Bool>
     }
 
     struct Output {
+        let searchUserList: PublishRelay<[User]>
+        let myRank: PublishRelay<(User, Int?)>
         let userList: PublishRelay<[User]>
     }
 
     func transform(_ input: Input) -> Output {
+        let searchUserList = PublishRelay<[User]>()
+        let myRank = PublishRelay<(User, Int?)>()
         let userList = PublishRelay<[User]>()
+
         let info = Driver.combineLatest(input.name, input.schoolId, input.dateType)
+        let mySchoolType = Driver.combineLatest(input.switchOn, input.dateType)
+        let anotherSchoolType = Driver.combineLatest(input.schoolId, input.dateType)
 
         input.name.asObservable().withLatestFrom(info).flatMap { name, id, type in
             self.searchUserUseCase.excute(schoolId: id, name: name, dateType: type)
         }.subscribe(onNext: {
-            userList.accept($0)
+            searchUserList.accept($0)
         }).disposed(by: disposeBag)
 
-        return Output(userList: userList)
+        input.switchOn.asObservable().withLatestFrom(mySchoolType).flatMap {
+            self.fetchUserSchoolRankUseCase.excute(scope: $0, dateType: $1)
+        }.subscribe(onNext: { data, walkCount in
+            myRank.accept((data.myRank, walkCount))
+            userList.accept(data.rankList)
+        }).disposed(by: disposeBag)
+
+        input.isMySchool.asObservable().subscribe(onNext: {
+            if $0 {
+                input.dateType.asObservable().withLatestFrom(mySchoolType).flatMap {
+                    self.fetchUserSchoolRankUseCase.excute(scope: $0, dateType: $1)
+                }.subscribe(onNext: { data, walkCount in
+                    myRank.accept((data.myRank, walkCount))
+                    userList.accept(data.rankList)
+                }).disposed(by: self.disposeBag)
+            } else {
+                input.dateType.asObservable().withLatestFrom(anotherSchoolType).flatMap {
+                    self.fetchUserRankUseCase.excute(schoolId: $0, dateType: $1)
+                }.subscribe(onNext: {
+                    userList.accept($0)
+                }).disposed(by: self.disposeBag)
+            }
+        }).disposed(by: disposeBag)
+
+        return Output(
+            searchUserList: searchUserList,
+            myRank: myRank,
+            userList: userList
+        )
     }
 }
