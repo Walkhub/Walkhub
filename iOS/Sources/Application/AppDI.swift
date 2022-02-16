a7c074491e37bfd43d51a726306f9088a37b0fd9
kimxwan <kimxwan0319@naver.com>
2022-02-17 06:31:24 +0900

    ⚡️ :: Home VC/VM register

--- iOS/Sources/Application/AppDI.swift
+++ iOS/Sources/Application/AppDI.swift
@@ -15,6 +15,14 @@ extension Container {
         self.register(LoginViewModel.self) { resolver in
             LoginViewModel(signinUseCase: resolver.resolve(SigninUseCase.self)!)
         }
+        self.register(HomeViewModel.self) { resolver in
+            HomeViewModel(
+                fetchCaloriesLevelUseCase: resolver.resolve(FetchCalroiesLevelUseCase.self)!,
+                fetchLiveDailyExerciseRecordUseCase: resolver.resolve(FetchLiveDailyExerciseRecordUseCase.self)!,
+                fetchExercisesAnalysisUseCase: resolver.resolve(FetchExerciseAnalysisUseCase.self)!,
+                fetchRankPreviewUseCase: resolver.resolve(FetchRankPreviewUseCase.self)!
+            )
+        }
     }
 
     private func registerViewController() {
@@ -22,6 +30,11 @@ extension Container {
         self.register(LoginViewController.self) { resolver in
             return LoginViewController().then {
                 $0.viewModel = resolver.resolve(LoginViewModel.self)!
+            }
+        }
+        self.register(HomeViewController.self) { resolver in
+            return HomeViewController().then {
+                $0.viewModel = resolver.resolve(HomeViewModel.self)!
             }
         }
     }
