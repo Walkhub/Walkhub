f4c1cc274a26542f07f3c77679f9911fc8b6afbe
kimxwan <kimxwan0319@naver.com>
2022-02-16 09:44:43 +0900

    ⚡️ :: UserSinginResponseDTO

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/Auth/UserSinginResponseDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/Auth/UserSinginResponseDTO.swift
@@ -1,13 +1,21 @@
 import Foundation
 
 // MARK: - Data Transfer Object
-struct TokenDTO: Decodable {
+struct UserSinginResponseDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case expiredAt = "expired_at"
         case refreshToken = "refresh_token"
+        case authority
+        case height
+        case weight
+        case sex
     }
     let accessToken: String
     let expiredAt: String
     let refreshToken: String
+    let authority: String
+    let height: Double
+    let weight: Int
+    let sex: String
 }
