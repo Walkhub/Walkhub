ac5369f3bb1b403e3c60bcc24dc9e2f5d7929580
rlarldud1234 <rlarldud6482@naver.com>
2022-02-19 02:53:26 +0900

    ⚡️ :: Color Assets

--- iOS/Sources/Presentation/Extension/UIColor+ColorAssets.swift
+++ iOS/Sources/Presentation/Extension/UIColor+ColorAssets.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 extension UIColor {
+    static let color757575 = UIColor(named: "Color757575")!
+    static let color616161 = UIColor(named: "Color616161")!
+    static let colorE0E0E0 = UIColor(named: "ColorE0E0E0")!
+    static let colorBDBDBD = UIColor(named: "ColorBDBDBD")!
     static let gray50 = UIColor(named: "Gray50")!
     static let gray100 = UIColor(named: "Gray100")!
     static let gray200 = UIColor(named: "Gray200")!
