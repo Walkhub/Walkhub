06f47dd22a8c5df8c468cc98363a3f8f68b309cd
rlarldud1234 <rlarldud6482@naver.com>
2022-02-19 18:46:22 +0900

    ⚡️ :: 휴대폰 사진앱 접근하고 다른 버튼, 딜리게이트 세팅

--- iOS/Sources/Presentation/Scene/Setting/EditProfileViewController.swift
+++ iOS/Sources/Presentation/Scene/Setting/EditProfileViewController.swift
@@ -2,8 +2,34 @@ import UIKit
 
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 class EditProfileViewController: UIViewController {
+
+    private let imagePickerView = UIImagePickerController()
+
+    private var disposeBag = DisposeBag()
+
+    private let searchController = UISearchController(searchResultsController: nil).then {
+        $0.searchBar.placeholder = "학교 이름 검색하기"
+    }
+
+    private let alertBackView = UIView().then {
+        $0.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.4)
+    }
+
+    private let alert = CustomAlert().then {
+        $0.setup(
+            title: "학교 변경 시 기존 소속 중인 반에서\n자동 탈퇴됩니다.",
+            cancel: "안하기",
+            ok: "변경하기")
+    }
+
+    private let schoolListTableView = UITableView().then {
+        $0.separatorStyle = .singleLine
+        $0.register(SchoolListTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
 
     private let profileImgView = UIImageView().then {
         $0.contentMode = .scaleToFill
@@ -28,7 +54,7 @@ class EditProfileViewController: UIViewController {
     }
 
     private let editNameBtn = UIButton(type: .system).then {
-        $0.setImage(.init(systemName: "clock.fill"), for: .normal)
+        $0.setImage(.init(named: "editImg"), for: .normal)
     }
 
     private let schoolInformationView = UIView().then {
@@ -47,7 +73,7 @@ class EditProfileViewController: UIViewController {
     }
 
     private let editSchoolInformationBtn = UIButton(type: .system).then {
-        $0.setImage(.init(systemName: "clock.fill"), for: .normal)
+        $0.setImage(.init(named: "editImg"), for: .normal)
     }
 
     private let editBtn = UIButton(type: .system).then {
@@ -59,7 +85,11 @@ class EditProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePickerView.delegate = self
+        nameTextField.delegate = self
+        searchController.searchBar.delegate = self
         demoData()
+        setBtn()
     }
 
     override func viewDidLayoutSubviews() {
@@ -69,10 +99,36 @@ class EditProfileViewController: UIViewController {
         editProfileImageBtn.layer.cornerRadius = editProfileImageBtn.frame.size.height / 2
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        schoolListTableView.isHidden = true
+        alertBackView.isHidden = true
+        alert.isHidden = true
+    }
+
     private func demoData() {
         nameTextField.text = "김기영"
         schoolLabel.text = "대덕소프트웨어마이스터고등학교"
         gradeClassLabel.text = "2학년 1반"
+    }
+
+    private func setBtn() {
+        editProfileImageBtn.rx.tap.subscribe(onNext: {
+            self.openPhotoLibrary()
+        }).disposed(by: disposeBag)
+
+        editNameBtn.rx.tap.subscribe(onNext: {
+            self.nameTextField.isEnabled = true
+            self.nameTextField.becomeFirstResponder()
+        }).disposed(by: disposeBag)
+
+        editSchoolInformationBtn.rx.tap.subscribe(onNext: {
+            self.navigationItem.searchController = self.searchController
+            Observable<Int>.interval(.seconds(0), scheduler: MainScheduler.instance)
+                .subscribe(onNext: { _ in
+                    self.searchController.searchBar.searchTextField.becomeFirstResponder()
+                }).disposed(by: self.disposeBag)
+            self.schoolListTableView.isHidden = false
+        }).disposed(by: disposeBag)
     }
 }
 
@@ -80,7 +136,8 @@ class EditProfileViewController: UIViewController {
 extension EditProfileViewController {
     private func addSubviews() {
         [profileImgView, editProfileImageBtn, nameView,
-         schoolInformationView, editBtn].forEach { view.addSubview($0) }
+         schoolInformationView, editBtn, alertBackView, alert, schoolListTableView]
+            .forEach { view.addSubview($0) }
 
         [nameTextField, editNameBtn].forEach { nameView.addSubview($0) }
 
@@ -142,5 +199,51 @@ extension EditProfileViewController {
             $0.leading.trailing.bottom.equalToSuperview()
             $0.height.equalTo(52)
         }
+
+        alertBackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        alert.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(296)
+            $0.height.equalTo(148)
+        }
+
+        schoolListTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension EditProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    private func openPhotoLibrary() {
+        imagePickerView.sourceType = .photoLibrary
+        present(imagePickerView, animated: false, completion: nil)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImgView.image = image
+        }
+
+        imagePickerView.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension EditProfileViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.isEnabled = false
+    }
+}
+
+extension EditProfileViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        schoolListTableView.isHidden = true
+        self.navigationItem.searchController = nil
     }
 }
