9e95403f32352937e4b7cfea632ae331a88e4b7c
kimxwan <kimxwan0319@naver.com>
2022-02-23 00:25:32 +0900

    ⚡️ :: 타임존 세팅

--- Service/Sources/Data/Util/DateUtil.swift
+++ Service/Sources/Data/Util/DateUtil.swift
@@ -3,15 +3,13 @@ import Foundation
 extension String {
     func toDate() -> Date {
         let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: TimeZone.current.identifier)
-        formatter.timeZone = TimeZone(identifier: TimeZone.current.identifier)
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter.date(from: self)!
     }
     func toDateWithTime() -> Date {
         let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: TimeZone.current.identifier)
-        formatter.timeZone = TimeZone(identifier: TimeZone.current.identifier)
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         return formatter.date(from: self)!
     }
@@ -20,11 +18,15 @@ extension String {
 extension Date {
     func toDateWithTimeString() -> String {
         let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: TimeZone.current.identifier)
+        formatter.timeZone = TimeZone(identifier: TimeZone.current.identifier)
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         return formatter.string(from: self)
     }
     func toDateString() -> String {
         let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: TimeZone.current.identifier)
+        formatter.timeZone = TimeZone(identifier: TimeZone.current.identifier)
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter.string(from: self)
     }
