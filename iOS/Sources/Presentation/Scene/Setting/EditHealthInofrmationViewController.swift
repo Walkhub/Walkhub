c5db0e5cd558567ad30863df585b38093f092b4b
rlarldud1234 <rlarldud6482@naver.com>
2022-02-25 00:10:50 +0900

    ⚡️ :: 건강정보 수정 뷰컨트롤러 생성

---
+++ iOS/Sources/Presentation/Scene/Setting/EditHealthInofrmationViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+import SnapKit
+import Then
+
+class EditHealthInofrmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
