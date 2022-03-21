ae43ece0622576e9f7483f083e2fc1fc53a9b5aa
rlarldud1234 <rlarldud6482@naver.com>
2022-03-21 22:20:47 +0900

    ⚡️ :: Model Entity school_id 추가

--- Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/User/UserProfileDTO.swift
+++ Service/Sources/Data/RemoteDataSource/RestAPI/DataMapping/User/UserProfileDTO.swift
@@ -7,6 +7,7 @@ struct UserProfileDTO: Decodable {
         case name
         case profileImageUrlString = "profile_image_url"
         case school = "school_name"
+        case schoolId = "school_id"
         case schoolImageUrlString = "school_image_url"
         case grade
         case classNum = "class_num"
@@ -16,7 +17,8 @@ struct UserProfileDTO: Decodable {
     let userID: Int
     let name: String
     let profileImageUrlString: String
-    let school: String?
+    let school: String
+    let schoolId: Int
     let schoolImageUrlString: String
     let grade: Int?
     let classNum: Int?
@@ -31,7 +33,8 @@ extension UserProfileDTO {
             userID: userID,
             name: name,
             profileImageUrl: URL(string: profileImageUrlString)!,
-            school: school ?? "",
+            school: school,
+            schoolId: schoolId,
             schoolImageUrl: URL(string: schoolImageUrlString)!,
             grade: grade ?? 0,
             classNum: classNum ?? 0,
