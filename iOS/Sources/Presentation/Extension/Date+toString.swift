87b490a27d6a9dae3c7ace765f7e890a02f49d84
sian7563 <sian7563@naver.com>
2022-03-31 15:56:02 +0900

    ⚡️ :: Extension Date Challenge에서 필요한 formatting 설정

--- iOS/Sources/Presentation/Extension/Date+toString.swift
+++ iOS/Sources/Presentation/Extension/Date+toString.swift
@@ -8,4 +8,12 @@ extension Date {
         formatter.dateFormat = "M.dd"
         return formatter.string(from: self)
     }
+
+    func challengeToString() -> String {
+        let formatter = DateFormatter()
+        formatter.locale =  Locale(identifier: "UTC")
+        formatter.timeZone = TimeZone(abbreviation: "KST")
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.string(from: self)
+    }
 }
