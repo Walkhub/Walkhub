d73976ee61b03947b3b34bcbf9220a669c6ab437
rlarldud1234 <rlarldud6482@naver.com>
2022-04-01 12:03:41 +0900

    ⚡️ :: SearchSchoolUseCase 설정

--- Service/Sources/Domain/Repository/SchoolRepository.swift
+++ Service/Sources/Domain/Repository/SchoolRepository.swift
@@ -3,6 +3,6 @@ import Foundation
 import RxSwift
 
 protocol SchoolRepository {
-    func searchSchool(name: String) -> Single<[SearchSchool]>
+    func searchSchool(name: String) -> Observable<[SearchSchool]>
     func fetchSchoolDetails(schoolId: Int) -> Single<SchoolDetails>
 }
