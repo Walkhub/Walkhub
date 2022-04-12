9bacb28d8314fbfdc663b5be5d065399bcbd3933
rlarldud1234 <rlarldud6482@naver.com>
2022-04-12 21:21:37 +0900

    ⚡️ :: 중복된 extension 삭제하기

--- iOS/Sources/Presentation/Extension/UITextField + addLeftPadding.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UITextField {
-  func addLeftPadding() {
-    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
-    self.leftView = paddingView
-    self.leftViewMode = ViewMode.always
-  }
-}
