88353827067c1da60852ccafbcf37d450cda4160
sian7563 <sian7563@naver.com>
2022-04-21 22:43:33 +0900

    ⚡️ :: ViewController 수정

--- iOS/Sources/Presentation/Scene/ChallengeScene/ChallengeViewController.swift
+++ iOS/Sources/Presentation/Scene/ChallengeScene/ChallengeViewController.swift
@@ -13,6 +13,7 @@ class ChallengeViewController: UIViewController {
 
     private let getData = PublishRelay<Void>()
     private var challengeList = [Challenge]()
+    private var joinedChallengeList = [JoinedChallenge]()
 
     private let challengeTableView = UITableView(frame: .zero, style: .insetGrouped).then {
         $0.backgroundColor = .gray50
@@ -52,11 +53,17 @@ class ChallengeViewController: UIViewController {
         )
 
         let output = viewModel.transform(input)
-
         output.challengList.asObservable().subscribe(onNext: {
+            print($0)
             self.challengeList = $0
             self.challengeTableView.reloadData()
         }).disposed(by: disposeBag)
+
+        output.joinedChallengeList.asObservable()
+            .subscribe(onNext: {
+                print($0)
+                self.joinedChallengeList = $0
+            }).disposed(by: disposeBag)
     }
 }
 extension ChallengeViewController: UITableViewDataSource, UITableViewDelegate {
@@ -65,15 +72,36 @@ extension ChallengeViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return challengeList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "participatingChallengeCell", for: indexPath) as? ParticipatingChallengeTableViewCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "participatingChallengeCell",
+                for: indexPath
+            ) as? ParticipatingChallengeTableViewCell
+            cell?.challengeTitleLabel.text = challengeList[indexPath.row].name
+            cell?.organizerLable.text = challengeList[indexPath.row].writer.name
+            cell?.dateLabel.text = "\(challengeList[indexPath.row].start) ~ \(challengeList[indexPath.row].end)"
+            cell?.schoolLogoImageView.kf.setImage(with: challengeList[indexPath.row].writer.profileImageUrl)
+            if (challengeList[indexPath.row].goalType.rawValue == "DISTANCE") {
+                cell?.presentStepCountLabel.text = "현재 \(challengeList[indexPath.row].goal)km"
+            } else {
+                cell?.presentStepCountLabel.text = "현재 \(challengeList[indexPath.row].goal)걸음"
+            }
+            if (challengeList[indexPath.row].goalType.rawValue == "DISTANCE") {
+                cell?.stepCountLabel.text = "\(challengeList[indexPath.row].goal)km"
+            } else {
+                cell?.stepCountLabel.text = "\(challengeList[indexPath.row].goal)걸음"
+            }
+
             return cell!
         } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "wholeChallengeCell", for: indexPath) as? WholeChallengeTableViewCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "wholeChallengeCell",
+                for: indexPath
+            ) as? WholeChallengeTableViewCell
             cell?.challengeTitleLabel.text = challengeList[indexPath.row].name
             cell?.schoolLogoImageView.kf.setImage(with: challengeList[indexPath.row].writer.profileImageUrl)
             return cell!
