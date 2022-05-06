d3c32ac7668b6f496e1c5a46d9cd589e90689ad7
rlarldud1234 <rlarldud6482@naver.com>
2022-05-07 03:13:40 +0900

    ⚡️ :: ViewModel 유즈케이스를 가져다가 사용

--- iOS/Sources/Presentation/Scene/SettingScene/AccountInformation/AccountInformationViewModel.swift
+++ iOS/Sources/Presentation/Scene/SettingScene/AccountInformation/AccountInformationViewModel.swift
@@ -3,19 +3,42 @@ import Foundation
 import RxSwift
 import RxCocoa
 import RxFlow
+import Service
 
 class AccountInformationViewModel: ViewModelType, Stepper {
+    private let fetchAccountInfoUseCase: FetchAccountInfoUseCase
+
+    init(fetchAccountInfoUseCase: FetchAccountInfoUseCase) {
+        self.fetchAccountInfoUseCase = fetchAccountInfoUseCase
+    }
 
     private var disposeBag = DisposeBag()
     var steps = PublishRelay<Step>()
 
     struct Input {
+        let getData: Driver<Void>
+    let changePwButtonDidTap: Driver<Void>
     }
 
     struct Output {
+        let accountInfo: PublishRelay<AccountInfo>
     }
 
     func transform(_ input: Input) -> Output {
-        return Output()
+        let accountInfo = PublishRelay<AccountInfo>()
+
+        input.getData.asObservable()
+            .flatMap {
+                self.fetchAccountInfoUseCase.excute()
+            }.subscribe(onNext: {
+                accountInfo.accept($0)
+            }).disposed(by: disposeBag)
+
+        input.changePwButtonDidTap.asObservable()
+            .map { WalkhubStep.checkPasswordScene }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+
+        return Output(accountInfo: accountInfo)
     }
 }
