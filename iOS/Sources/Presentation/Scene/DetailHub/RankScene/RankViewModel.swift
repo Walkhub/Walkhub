9175a7891be801a5e0d2986d22bf1de6c8cd58e5
rlarldud1234 <rlarldud6482@naver.com>
2022-05-23 18:55:54 +0900

    ⚡️ :: RankViewModel 필요없는 유즈케이스 삭제

--- iOS/Sources/Presentation/Scene/DetailHub/RankScene/RankViewModel.swift
+++ iOS/Sources/Presentation/Scene/DetailHub/RankScene/RankViewModel.swift
@@ -8,37 +8,59 @@ import Service
 class RankViewModel: ViewModelType, Stepper {
 
     private let fetchMySchoolUserRankUseCase: FetchMySchoolUserRankUseCase
-    private let fetchAnotherSchoolUserRankUseCase: FetchAnotherSchoolUserRankUseCase
 
     init(
-        fetchMySchoolUserRankUseCase: FetchMySchoolUserRankUseCase,
-        fetchAnotherSchoolUserRankUseCase: FetchAnotherSchoolUserRankUseCase
+        fetchMySchoolUserRankUseCase: FetchMySchoolUserRankUseCase
     ) {
         self.fetchMySchoolUserRankUseCase = fetchMySchoolUserRankUseCase
-        self.fetchAnotherSchoolUserRankUseCase = fetchAnotherSchoolUserRankUseCase
     }
 
     var steps = PublishRelay<Step>()
     private var disposeBag = DisposeBag()
 
     struct Input {
-        let getData: Driver<Void>
-        let schoolId: Int?
+        let schoolId: Int
         let dateType: Driver<DateType>
         let groupScope: Driver<GroupScope>
     }
     struct Output {
         let myRank: PublishRelay<(RankedUser, Int?)>
         let userRankList: PublishRelay<[RankedUser]>
+        let isJoined: PublishRelay<Bool>
     }
 
     func transform(_ input: Input) -> Output {
         let myRank = PublishRelay<(RankedUser, Int?)>()
         let userRankList = PublishRelay<[RankedUser]>()
+        let isJoined = PublishRelay<Bool>()
+        let mySchoolInfo = Driver.combineLatest(input.groupScope, input.dateType)
+
+        input.groupScope
+            .asObservable()
+            .withLatestFrom(mySchoolInfo)
+            .flatMap {
+                self.fetchMySchoolUserRankUseCase.excute(scope: $0, dateType: $1)
+            }.subscribe(onNext: {
+                myRank.accept(($0.0.myRank, $0.1))
+                userRankList.accept($0.0.rankList)
+                isJoined.accept($0.0.isJoinedClass)
+            }).disposed(by: disposeBag)
+
+        input.dateType
+            .asObservable()
+            .withLatestFrom(mySchoolInfo)
+            .flatMap {
+                self.fetchMySchoolUserRankUseCase.excute(scope: $0, dateType: $1)
+            }.subscribe(onNext: {
+                myRank.accept(($0.0.myRank, $0.1))
+                userRankList.accept($0.0.rankList)
+                isJoined.accept($0.0.isJoinedClass)
+            }).disposed(by: disposeBag)
 
         return Output(
             myRank: myRank,
-            userRankList: userRankList
+            userRankList: userRankList,
+            isJoined: isJoined
         )
     }
 }
