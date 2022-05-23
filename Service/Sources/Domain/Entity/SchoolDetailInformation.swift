4019ad40abce282323a576b35d5bd2224f411210
rlarldud1234 <rlarldud6482@naver.com>
2022-05-23 19:01:01 +0900

    ⚡️ :: Domain Entity

---
+++ Service/Sources/Domain/Entity/SchoolDetailInformation.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct SchoolDetailInformtaion: Equatable {
+    public let totalWalkCount: Int
+    public let date: Date
+    public let totalUserCount: Int
+    public let ranking: Int
+}
