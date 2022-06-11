01777bed592710831ad970cfa3dec7b92016e87b
rlarldud1234 <rlarldud6482@naver.com>
2022-06-11 22:47:53 +0900

    ⚡️ :: 충돌난 부분 삭제하기

--- Service/Sources/Domain/Repository/UserRepository.swift
+++ Service/Sources/Domain/Repository/UserRepository.swift
@@ -9,12 +9,7 @@ protocol UserRepository {
     func fetchMyProfile() -> Observable<UserProfile>
     func changeProfile(name: String, profileImageUrlString: String, schoolId: Int) -> Completable
     func setHealthInformation(height: Double?, weight: Int?, sex: Sex) -> Completable
-<<<<<<< HEAD
-    func joinClass(sectionId: Int, classCode: String, num: Int) -> Single<Void>
-=======
     func joinClass(classCode: String, num: Int) -> Completable
-    func setSchoolInformation(schoolId: Int) -> Single<Void>
->>>>>>> ff0db01b3db379d9c73b7b18b0e6b29b58ed9f34
     func changeGoalWalkCount(goalWalkCount: Int) -> Single<Void>
     func fetchUserHealth() -> Single<UserHealth>
     func checkClassCode(code: String) -> Completable
